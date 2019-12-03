28e3121dd0181a91c2c4d71b1745c657b103ed4b
Tarik Ünal <2108287@talnet.nl>
2019-12-03 14:59:09 +0100

    loops 1 en 2

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+var i;
+var tekst="";
+
+for (let i = 1; i <=5; i++) {
+        tekst += i + "<br>";
+        console.log(i);
+
+}
+
+document.getElementById("uitvoer").innerHTML=tekst;
--- loops_opdrachten/loops_opdracht_2/js/script.js
+++ loops_opdrachten/loops_opdracht_2/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+var i;
+var tekst="";
+
+for (let i = 20; i >0; i--) {
+    tekst += i + "<br>";
+    console.log(i);
+
+}
+
+document.getElementById("uitvoer").innerHTML=tekst;
