de5245bde2b14f61f0d7fde20af035f233fba01b
Tarik Ünal <2108287@talnet.nl>
2019-12-03 13:50:11 +0100

    opdracht 7

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,19 @@
 //javascript code
+function achtergrond(){ 
+var getal = (document.getElementById("getal").value);
+
+    
+    if(getal<50){ 
+        document.getElementById("body").style.backgroundColor = "blue";
+
+
+        
+    }else if(getal==50){
+            document.getElementById("body").style.backgroundColor = "red";
+    }
+    else{
+        document.getElementById("body").style.backgroundColor = "green";
+    }
+    
+
+}
