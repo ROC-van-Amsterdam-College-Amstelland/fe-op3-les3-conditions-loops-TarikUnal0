da71ec2ea61311a8c8252be5a4bf6ae545c774e9
Tarik Ünal <2108287@talnet.nl>
2019-12-03 15:26:38 +0100

    opdracht loops 3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,17 @@
 //javascript code
+var i;
+var tekst="";
+
+for (let i = 1; i <=50; i++) {
+    
+    if (i==20 || i==50 ) {
+        tekst += i + "dit is een mooi getal<br>";
+        
+    } else {
+       tekst += i + "<br>";
+}
+    console.log(i);
+
+}
+
+document.getElementById("uitvoer").innerHTML=tekst;
