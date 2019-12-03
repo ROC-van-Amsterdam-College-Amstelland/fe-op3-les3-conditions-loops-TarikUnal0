d42ade0f2dd57e9232296ce08adfc0852f7bbbc1
Tarik Ünal <2108287@talnet.nl>
2019-12-03 13:03:34 +0100

    opdracht 5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,16 @@
 //javascript code
+function bereken(){
+
+var getal1 = parseInt(document.getElementById("getal1").value);
+var getal2 = parseInt(document.getElementById("getal2").value);
+var operator = document.getElementById("operator").value;
+
+if(operator=="+"){ 
+    document.getElementById("antwoord").innerHTML = getal1 + getal2
+
+} else {
+    document.getElementById("antwoord").innerHTML = getal1 - getal2
+
+}
+
+}
