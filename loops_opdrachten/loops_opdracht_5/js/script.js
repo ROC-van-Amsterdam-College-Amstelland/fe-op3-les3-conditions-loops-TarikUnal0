a767afcfcd92f87d41baea60a3dcad810badfd5e
Tarik Ünal <2108287@talnet.nl>
2019-12-09 14:29:43 +0100

    opdr 5

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,11 @@
-//javascript code
+var tekst = ""
+for (let i = 0; i <= 20; i+= 1) {
+    rest = i % 2;
+    if (rest == 0) {
+        tekst += i + "<br>";
+    } else {
+        console.log(i)
+    }
+    
+}
+document.getElementById("uitkomst").innerHTML=tekst;
